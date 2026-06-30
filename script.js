98eda51182f913a9c34b8e2810cfa87dc7b4a1ec
clararebeca <notclararebeca@gmail.com>
2026-06-29 23:46:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Definição da função
+function exibirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Execução da função 3 vezes
+exibirMensagem();
+exibirMensagem();
+exibirMensagem();
